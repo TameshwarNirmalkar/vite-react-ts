c430f1df2e7417f9b880bd5895a99e53849ecd8a
Tameshwar Nirmalkar <5547424+TameshwarNirmalkar@users.noreply.github.com>
2022-05-30 23:06:19 +0530

    added api

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 import { Header } from 'components/Header';
 import { useEffect, useState } from 'react';
-import { getPractitioners } from 'service/rest_api';
+import { getAppointments, getPractitioners } from 'service/rest_api';
 
 interface PractionersI {
   id: number;
   name: string;
 }
 
+interface AppointmentsI {
+  id: number;
+  revenue: number;
+  cost: number;
+  practitioner_id: number;
+  date: string;
+}
+
 function App() {
-  const [practionerList, setPractionerList]: any = useState([]);
+  const [practionerList, setPractionerList]: Array<any> = useState([]);
+  const [appointmentList, setAppointmentList]: Array<any> = useState([]);
 
   const getPractitionersApi = async () => {
     const res: Array<PractionersI> = await getPractitioners();
     setPractionerList(res);
   };
 
+  const getAppointmentsApi = async () => {
+    const res: Array<AppointmentsI> = await getAppointments();
+    setAppointmentList(res);
+  };
+
   useEffect(() => {
     getPractitionersApi();
+    getAppointmentsApi();
     return () => {
       console.log('Unmount');
     };
   }, []);
 
-  const onRowClick = (item: PractionersI) => {
+  const onRowClick = (item: AppointmentsI) => {
     console.log(item);
   };
 
@@ -30,24 +45,57 @@ function App() {
     <div className="App">
       <Header title="Practitioner" />
       <div className="container my-4">
+        <div className="container">
+          <div className="row align-items-end">
+            <div className="col">
+              <label className="form-label">Practitioner</label>
+              <select className="form-select" placeholder="Choose Practitioner">
+                {practionerList.map((el: PractionersI, i: number) => (
+                  <option key={i} value={el.id}>
+                    {el.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col">
+              <label className="form-label">From</label>
+              <input type="date" className="form-control" />
+            </div>
+            <div className="col">
+              <label className="form-label">To</label>
+              <input type="date" className="form-control" />
+            </div>
+            <div className="col">
+              <button type="button" className="btn btn-primary text-dark">
+                Generate Report
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container my-4">
         <div className="card">
           <div className="card-body">
             <table className="table table-striped">
               <thead>
                 <tr>
                   <th>
-                    <strong>#ID</strong>
+                    <strong>Date</strong>
                   </th>
                   <th>
-                    <strong>Name</strong>
+                    <strong>Revenue</strong>
+                  </th>
+                  <th>
+                    <strong>Cost</strong>
                   </th>
                 </tr>
               </thead>
               <tbody>
-                {practionerList.map((el: PractionersI, i: number) => (
+                {appointmentList.map((el: AppointmentsI, i: number) => (
                   <tr key={i} onClick={() => onRowClick(el)} role="button">
-                    <td>{el.id}</td>
-                    <td>{el.name}</td>
+                    <td>{el.date}</td>
+                    <td>{el.revenue}</td>
+                    <td>{el.cost}</td>
                   </tr>
                 ))}
               </tbody>
